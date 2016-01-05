d124274cafc657923aa4706049bb917dc80ee13a
shashidharhr <shashidhar.hr@gmail.com>
2016-01-05 12:55:16 +0530

    Finish log-in/log-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
 
 
+
 resources :users
+
+
+  get 'login' =>  'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+
   get 'signup' => 'users#new'
 
    root 'static_pages#home'
