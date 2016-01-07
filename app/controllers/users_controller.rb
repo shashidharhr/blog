2d0b9454b1b11417f076cb3d6785312597f1104c
shashidharhr <shashidhar.hr@gmail.com>
2016-01-07 10:20:53 +0530

    finished updating users fuinctionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
       @user = User.new
   end
@@ -21,6 +23,21 @@ class UsersController < ApplicationController
  end
 
 
+ def edit
+     @user = User.find(params[:id])
+   end
+
+
+   def update
+   @user = User.find(params[:id])
+   if @user.update_attributes(user_params)
+     flash[:success] = "Profile updated"
+     redirect_to @user
+   else
+     render 'edit'
+   end
+ end
+
 
 private
 
@@ -29,7 +46,15 @@ private
                                  :password_confirmation)
   end
 
+  # Before filters
 
+      # Confirms a logged-in user.
+      def logged_in_user
+        unless logged_in?
+          flash[:danger] = "Please log in."
+          redirect_to login_url
+        end
+      end
 
 
 
